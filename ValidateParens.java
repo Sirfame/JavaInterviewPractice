685e0574d771f7c7bd9e6c0d37997bb8ae37cfa5
Sirfame Lin <sifame@hotmail.com>
2016-07-29 10:40:03 -0700

    Longest valid parens, returns int

--- ValidateParens.java
+++ ValidateParens.java
@@ -22,12 +22,49 @@ public class ValidateParens {
          }
          return true;
        }
+       
+       public static int longestValidParens(String input) {
+         Stack<Character> stack = new Stack<Character>();
+         int largest = 0;
+         int count = 0;
+         char[] charArray = input.toCharArray();
+         for(int i = 0; i < charArray.length; i++) {            
+            if(charArray[i] == '(' || charArray[i] == '[' || charArray[i] == '{') {
+               stack.push(charArray[i]);
+            } else {
+               if(charArray[i] == ')' && stack.peek() != '(') {
+                  if(largest < count)
+                     largest = count;
+                  count = 0;
+               } else if(charArray[i] == ']' && stack.peek() != '[') {
+                  if(largest < count)
+                     largest = count;
+                  count = 0;
+               } else if(charArray[i] == '}' && stack.peek() != '{') {
+                  if(largest < count)
+                     largest = count;
+                  count = 0;
+               } else {
+                  count += 2;
+                  if(largest < count)
+                     largest = count;
+                  stack.pop();               
+               }
+            }
+         }
+         return largest;
+       }
+       
+       
 
    public static void main(String[] args) { 
        System.out.println(validBrackets("()()()"));
        System.out.println(validBrackets("({[({[]})]})"));
        System.out.println(validBrackets("())())"));
        System.out.println(validBrackets("{}{}{{{}}}{}{}[]()"));
-
+       System.out.println(longestValidParens("()()()"));
+       System.out.println(longestValidParens("(()"));
+       System.out.println(longestValidParens("{}{}{{{}}}{}{}[]()"));
+       
    }
 }
