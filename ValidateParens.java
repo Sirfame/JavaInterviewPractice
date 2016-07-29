d66ccc3e65d6c63ed8fdb1c87435890e4a190911
Sirfame Lin <sifame@hotmail.com>
2016-07-28 22:19:15 -0700

    reorg files

--- ValidateParens.java
+++ ValidateParens.java
@@ -1,30 +1,6 @@
 import java.util.*;
-public class test {
+public class ValidateParens {
 
-      public static int count_palindromes(String S) {
-           if(S.equals("")) {
-               return 0;
-           }
-           int count = 0;
-           for(int start = 0; start < S.length(); start++) {
-               for(int end = S.length(); end > start; end--) {
-                  if(isPalindrome(S.substring(start, end))) {
-                     count++;
-                  }
-               }
-           }
-            return count;
-       }
-   
-       public static boolean isPalindrome(String s) {
-           char[] charArray = s.toCharArray();
-           StringBuilder sb = new StringBuilder(charArray.length);
-           for(int i = charArray.length - 1; i >= 0; i--) {
-               sb.append(charArray[i]);
-           }
-           return s.equals(sb.toString());
-       }
-       
        public static boolean validBrackets(String input) {
          Stack<Character> stack = new Stack<Character>();
          char[] charArray = input.toCharArray();
---
+++ count_palindromes.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class count_palindromes {
+
+      public static int count_palindromes(String S) {
+           if(S.equals("")) {
+               return 0;
+           }
+           int count = 0;
+           for(int start = 0; start < S.length(); start++) {
+               for(int end = S.length(); end > start; end--) {
+                  if(isPalindrome(S.substring(start, end))) {
+                     count++;
+                  }
+               }
+           }
+            return count;
+       }
+   
+       public static boolean isPalindrome(String s) {
+           char[] charArray = s.toCharArray();
+           StringBuilder sb = new StringBuilder(charArray.length);
+           for(int i = charArray.length - 1; i >= 0; i--) {
+               sb.append(charArray[i]);
+           }
+           return s.equals(sb.toString());
+       }
+
+   public static void main(String[] args) { 
+      System.out.println(count_palindromes("helloelle"));
+
+   }
+}
