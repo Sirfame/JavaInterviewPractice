2e2360305a418482d6ba89b5cca0c241084cda0b
Sirfame Lin <sifame@hotmail.com>
2016-08-05 19:51:10 -0700

    conseqSequence

--- Interviews/src/minSubArraySum.java
+++ Interviews/src/minSubArraySum.java
@@ -4,7 +4,10 @@ public class minSubArraySum {
 
 	public static void main(String[] args) {
       int[] arr = {6, 3, 5, 8, 4, 2, 1};
+      int[] arr2 = {11, 13, 15, 16};
 		System.out.println(minSubArraySum(arr, 100));
+      System.out.println(consecSequence(arr));
+      System.out.println(consecSequence(arr2));
 	}
    
    public static int minSubArraySum(int[] arr, int s) {
@@ -19,6 +22,23 @@ public class minSubArraySum {
          count++;
       }
       return 0;
+   }
+   
+   public static int consecSequence(int[] arr) {
+      Arrays.sort(arr);
+      int consec = 0;
+      int currentConsec = 1;
+      for(int i = 1; i < arr.length; i++) {
+         if(arr[i - 1] == arr[i] - 1) {
+            currentConsec++;
+         } else {
+            currentConsec = 1;
+         }
+         if(currentConsec > consec) {
+            consec = currentConsec;
+         }
+      }
+      return consec;
    }
    
 }
