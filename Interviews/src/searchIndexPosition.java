e8392cf698fbad14bd023a090738545762fa42d2
Sirfame Lin <sifame@hotmail.com>
2016-08-16 14:41:45 -0700

    implemented binary search

--- Interviews/src/searchIndexPosition.java
+++ Interviews/src/searchIndexPosition.java
@@ -2,22 +2,34 @@
 public class searchIndexPosition {
 
 	public static void main(String[] args) {
-		int[] arr = {6, 3, 5, 8, 4, 2, 1};
-		int[] arr2 = {11, 13, 15, 16};
 		int[] arr3 = {1, 3, 5, 6};
 		System.out.println(indexPosition(arr3, 5));
 	    System.out.println(indexPosition(arr3, 2));
 	    System.out.println(indexPosition(arr3, 7));
 	    System.out.println(indexPosition(arr3, 0));
+	    System.out.println(indexPosition(arr3, 4));
 	}
 	
 	public static int indexPosition(int[] arr, int num) {
-	   for(int i = 0; i < arr.length; i++) {
-		   if(arr[i] >= num) {
-			   return i;
-		   }
-	   }
-	   return arr.length;
+		int low = 0;
+		int high = arr.length - 1;
+		int middle = 0;
+		while(low <= high) {
+			middle = (low + high) / 2;
+			if(arr[middle] == num) {
+				return middle;
+			}
+			if(arr[middle] > num) {
+				high = middle - 1;
+			} else {
+				low = middle + 1;
+			}
+		}
+		if(arr[middle] < num) {
+			return middle + 1;
+		} else {
+			return middle;
+		}
 	}
 		
 
